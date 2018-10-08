1695622b753ed11ec25e45569b57756a85862d70
cdleong <cdleong+github@gmail.com>
2018-10-08 19:56:15 -0400

    CDL:calculate_isothermal_atmo_scale_height

--- src/standard_atmosphere.py
+++ src/standard_atmosphere.py
@@ -4,8 +4,9 @@ Created on Oct 8, 2018
 @author: cdleong
 '''
 import pandas as pd #also need xlrd
+import pyphenom_physical_constants
 
-class atmo_calculator(object):
+class Atmosphere(object):
     standard_atmo_gas_names = "N2    O2    H2O    CO2    O3    N2O    CO    CH4    NO    SO2    NO2    NH3    HNO3    OH    HF    HCL    HBR    HI    CLO    OCS    H2CO    HOCL    HCN    CH3CL    H2O2    C2H2    C2H6    PH3".split()
     def __init__(self, file_path="../data/Atmosphere Model.xls"):
         self.file_path = file_path
@@ -42,14 +43,22 @@ class atmo_calculator(object):
 def hydrostatic_molecular_density_at_altitude(altitude):
     pass
 
-def calculate_effective_scale_height():
+def calculate_isothermal_atmo_scale_height(temp_kelvin, 
+                                           mean_molecular_mass_kg=4.79*10**-26):
+    """assuming isothermal and using an estimated mean molecular mass"""
     
+    k_b = float(pyphenom_physical_constants.STEFAN_BOLTZMANN_CONSTANT)
+    
+    g = pyphenom_physical_constants.GRAVITY_ON_EARTH   #"something something TODO"
+    
+    return k_b*temp_kelvin/(mean_molecular_mass_kg*g)
+
 
 if __name__ == '__main__':
        
-    bob = atmo_calculator()
-    print(atmo_calculator.standard_atmo_gas_names)
-    for b in atmo_calculator.standard_atmo_gas_names:
+    bob = Atmosphere()
+    print(Atmosphere.standard_atmo_gas_names)
+    for b in Atmosphere.standard_atmo_gas_names:
         print(b)
     
     
