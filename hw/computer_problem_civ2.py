06033b4234d6d177f73346f8858622b1c2030ded
Colin Leong <cdleong+github@gmail.com>
2018-10-30 00:28:23 -0400

    CDL: got linear interpolation working. Next to try joining/interpolating
    Signed-off-by: Colin Leong <cdleong+github@gmail.com>

--- hw/computer_problem_civ2.py
+++ hw/computer_problem_civ2.py
@@ -4,16 +4,12 @@
 """
 
 import os
-import geopandas
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
 
-from pyphenom import colorline
 
-
-from shapely.geometry import Point
-from shapely.geometry import shape
 
 dirname = os.path.dirname(__file__)
 
@@ -27,13 +23,14 @@ def load_data(event_data_file=dirname+"/../data/Formatted Event Data.xls",
     return event_data_df, altitude_data_df
 
 
-def plot_df_altitude_vs_time(times_sec, altitudes_km):
+def plot_df_altitude_vs_time(times_sec, altitudes_km, title="Altitude vs Time"):
     """Plot raw data from file."""
     keys = altitude_data_df.keys()
     print(keys)
     plt.figure()
-    plt.plot(times_sec, altitudes_km)
-    plt.title("Altitude vs Time")
+#    plt.plot(times_sec, altitudes_km)
+    plt.scatter(times_sec, altitudes_km)
+    plt.title(title)
     plt.xlabel("Time after 'launch' (sec)")
     plt.ylabel("altitude (km)")
 
@@ -41,14 +38,29 @@ def plot_df_altitude_vs_time(times_sec, altitudes_km):
 if __name__ == "__main__":
     event_data_df, altitude_data_df = load_data()
 
-    row_actual_data_starts = 1
+    units_row = 0
+    print(altitude_data_df)
+    altitude_data_df = altitude_data_df.drop([units_row])
+    print(altitude_data_df)
 
     raw_altitudes_km = altitude_data_df["altitude"]
-    raw_altitudes_km = raw_altitudes_km[row_actual_data_starts:]
 
     raw_times_sec = altitude_data_df["time"]
-    raw_times_sec = raw_times_sec[row_actual_data_starts:]
 
     plot_df_altitude_vs_time(times_sec=raw_times_sec,
                              altitudes_km=raw_altitudes_km)
+
+    raw_event_times_sec = event_data_df["TIME"]
+    print(raw_event_times_sec.shape)
+    print(type(raw_event_times_sec))
+    print(raw_altitudes_km.shape)
+
+    alt_km_interpolator = interp1d(raw_times_sec, raw_altitudes_km)
+
+    raw_event_times_sec_greater_than_zero = [time for time in raw_event_times_sec if time > 0]
+    linear_interpolated_altitudes_km = alt_km_interpolator(raw_event_times_sec_greater_than_zero)
+    plot_df_altitude_vs_time(times_sec=raw_event_times_sec_greater_than_zero,
+                             altitudes_km=linear_interpolated_altitudes_km,
+                             title="Linear interpolated altitude vs time")
+
     plt.show()
