bc002d6b1c9986f958b30bd194c3ad86d9002e92
Colin Leong <cdleong+github@gmail.com>
2018-11-27 20:08:08 -0500

    CDL: CIV-4 - checked dtypes on pandas dataframes
    Signed-off-by: Colin Leong <cdleong+github@gmail.com>

--- hw/computer_problem_civ4.py
+++ hw/computer_problem_civ4.py
@@ -21,13 +21,33 @@ def load_data(final_problem_data_file=dirname + "/../data/Final Problem.xls",
               atmo_trans_file=dirname + "/../data/ECE 595 Lesson 13 Transmission to Space.xlsx"):
     """Load excel files to pandas dataframes."""
     qe_df = pd.read_excel(final_problem_data_file, sheet_name="Quantum Efficiencies")
+    print("loaded QE sheet.")
     print(qe_df.head())
+    print("dtypes:")
+    print(qe_df.dtypes)
 
     tau_df = pd.read_excel(final_problem_data_file, sheet_name="Tau Optics")
+    print("\nLoaded TO sheet")
     print(tau_df.head())
+    print("dtypes:")
+    print(tau_df.dtypes)
 
     atmo_trans_df = pd.read_excel(atmo_trans_file)
+    print("\nloaded atmo trans sheet")
+    units_row = 0
+
+    # drop units row
+    atmo_trans_df = atmo_trans_df.drop([units_row])
+
+    # fix datatypes
+    for column_name in atmo_trans_df:
+
+        print("fixing dtype for column {}".format(column_name))
+        atmo_trans_df[column_name] = atmo_trans_df[column_name].apply(float)
+
     print(atmo_trans_df.head())
+    print("dtypes:")
+    print(atmo_trans_df.dtypes)
 
     return qe_df, tau_df, atmo_trans_df
 
